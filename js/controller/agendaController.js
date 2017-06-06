277ebaa457fc9406f3577f1f1ef8bec2359d43a5
vfbellaver <vfbellaver@gmail.com>
2017-06-05 20:00:12 -0400

    bib i18n, filter date

--- js/controller/agendaController.js
+++ js/controller/agendaController.js
@@ -2,19 +2,48 @@
  * Created by vfbeL on 17/05/2017.
  */
 
-angular.module("controlacasa").controller("agendaController",function ($scope,agendaAPIService) {
+angular.module("controlacasa").controller("agendaController",function ($scope,agendaAPIService,$location) {
     $scope.listaDeAgendas = [];
 
     $scope.cadastrarAgenda = function (agenda) {
-        agendaAPIService.salvarAgenda(agenda).then(function (dados) {
-            alert("Agenda salva com sucesso");
-            $location.url("/listarAgenda")
-        },function (err) {
-            alert("Deu erro ao Agendar"+err);
-        });
+        if(agenda.id){
+            agendaAPIService.atualizarAgenda(agenda).then(function (dados) {
+                alert("Agenda atualizada com sucesso");
+                $location.url("/listarAgenda")
+            },function (err) {
+                alert("Deu erro ao atualizar"+err);
+            });
+        }else{
+            agendaAPIService.salvarAgenda(agenda).then(function (dados) {
+                alert("Agenda salva com sucesso");
+                $location.url("/listarAgenda")
+            },function (err) {
+                alert("Deu erro ao Agendar"+err);
+            });
+        }
+        $scope.agenda = {};
     }
+
     $scope.imprimirDataEHorario = function (d) {
         alert(d.data)
+    }
+
+    $scope.editAgenda = function (agenda) {
+        var copia = {}
+        for (chave in agenda){
+            copia[chave] = agenda[chave];
+        };
+        $scope.agenda=copia;
+    }
+
+    $scope.deleteAgenda = function (id) {
+        agendaAPIService.deletarAgenda(id).then(function (dados) {
+            alert("Evento deletado com sucesso");
+            listarAgendas();
+            // $location.url("/listarAgenda")
+        },function (err) {
+            alert("Deu erro ao Deletar"+err.status);
+        });
     }
 
     var listarAgendas = function () {
@@ -27,6 +56,8 @@ angular.module("controlacasa").controller("agendaController",function ($scope,ag
         };
         agendaAPIService.listarAgenda().then(sucesso,erro);
     }
+
+
 
     listarAgendas();
 });
--- js/service/agendaAPIService.js
+++ js/service/agendaAPIService.js
@@ -10,6 +10,20 @@ angular.module("controlacasa").factory("agendaAPIService",function ($http) {
             data:dados
         });
     };
+    var _atualizarAgenda = function (dados) {
+        return $http({
+            method:"PUT",
+            url:"http://localhost:8084/WSRestControlaCasa/evento",
+            data:dados
+        });
+    };
+
+    var _deletarEvento = function (id) {
+        return $http({
+            method:"DELETE",
+            url:"http://localhost:8084/WSRestControlaCasa/evento/"+id,
+        });
+    };
 
     var _listarAgenda = function () {
         return $http({
@@ -20,6 +34,8 @@ angular.module("controlacasa").factory("agendaAPIService",function ($http) {
 
     return{
         listarAgenda: _listarAgenda,
-        salvarAgenda: _salvarAgenda
+        salvarAgenda: _salvarAgenda,
+        deletarAgenda: _deletarEvento,
+        atualizarAgenda: _atualizarAgenda
     }
 })
